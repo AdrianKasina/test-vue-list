0172b8b0ed5be8cb9dbf23bc04fc3766b35f636e
Adrian Kasina <kasina.adrian+1@gmail.com>
2019-10-16 22:28:21 +0100

    get list of post from api and displays as a list

--- list/src/App.vue
+++ list/src/App.vue
@@ -1,24 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HeaderTitle msg="List" />
+    <ListPosts />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderTitle from "./components/HeaderTitle.vue";
+import ListPosts from "./components/ListPosts.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    HeaderTitle,
+    ListPosts
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ list/src/components/ListPosts.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="posts">
+    <ul>
+      <li v-for="post in posts" :key="post.id">
+        <h3>{{ post.title }}</h3>
+        <p>{{ post.body }}</p>
+      </li>
+    </ul>
+    <!-- <ul>
+      <li v-for="item in items" :key="item.id">{{ item }}</li>
+    </ul>-->
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "ListPosts",
+  data() {
+    return {
+      posts: null
+    };
+  },
+  mounted() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then(response => (this.posts = response.data))
+      .catch(error => console.log(error)); // eslint-disable-line no-console
+  }
+};
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
