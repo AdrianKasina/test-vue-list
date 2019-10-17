34ae573d2c7244a2470292c56100d50a7f467f48
Adrian Kasina <kasina.adrian+1@gmail.com>
2019-10-17 11:43:55 +0100

    deleting a post from the list

--- list/src/components/ListPosts.vue
+++ list/src/components/ListPosts.vue
@@ -1,14 +1,13 @@
 <template>
   <div id="posts">
     <ul>
-      <li v-for="post in posts" :key="post.id">
+      <li v-for="post in posts" :key="post.index">
         <h3>{{ post.title }}</h3>
         <p>{{ post.body }}</p>
+        <button class="edit">Edit</button>
+        <button class="delete" @click="deletePost(post.index)">Delete</button>
       </li>
     </ul>
-    <!-- <ul>
-      <li v-for="item in items" :key="item.id">{{ item }}</li>
-    </ul>-->
   </div>
 </template>
 
@@ -19,31 +18,64 @@ export default {
   name: "ListPosts",
   data() {
     return {
-      posts: null
+      posts: []
     };
   },
-  mounted() {
+  created() {
     axios
       .get("https://jsonplaceholder.typicode.com/posts")
       .then(response => (this.posts = response.data))
       .catch(error => console.log(error)); // eslint-disable-line no-console
+  },
+
+  methods: {
+    deletePost: function(index) {
+      axios
+        .delete("https://jsonplaceholder.typicode.com/posts/" + index)
+        .then(() => {
+          this.posts.splice(index, 1);
+        });
+    }
   }
 };
 </script>
 
 <style scoped>
 h3 {
-  margin: 40px 0 0;
+  margin: 30px 0 0;
 }
 ul {
   list-style-type: none;
-  padding: 0;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
 }
+
 li {
-  display: inline-block;
-  margin: 0 10px;
+  width: 80%;
+  padding: 0 25px 15px;
+  border: 1px solid #f37061;
+  margin: 10px 0;
 }
 a {
   color: #42b983;
+}
+
+button {
+  min-width: 64px;
+  margin: 10px;
+  padding: 8px;
+  background: white;
+  text-transform: uppercase;
+}
+
+.edit {
+  border: 1px solid #32ba54;
+  color: #32ba54;
+}
+
+.delete {
+  border: 1px solid #de4710;
+  color: #de4710;
 }
 </style>
